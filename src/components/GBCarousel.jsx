69621720bdee58526560033a64da182d7f03079d
sillah2010 <macktastic2014@gmail.com>
2018-03-09 17:54:10 -0500

    Added new image and styling for carousel

--- src/components/GBCarousel.jsx
+++ src/components/GBCarousel.jsx
@@ -1,35 +1,42 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap'
-import ad_gb from '../imgs/ad_gb.jpg'
+import gbAd from '../imgs/gb_logo_bb.jpg'
+import styled from 'styled-components'
+
+const CarouselStyle = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+//920 × 642
 
 const GBCarousel = () => {
-
-    const gbAd = ad_gb;
-
     return (
-        <Carousel>
-            <Carousel.Item>
-                    <img id="carouselPic" alt="GB Monthly Ad" src={gbAd} />
-                    <Carousel.Caption>
+        <CarouselStyle>
+            <Carousel>
+                <Carousel.Item>
+                    <img className="carouselPic" alt="GB Monthly Ad" src={gbAd} />
+                    <Carousel.Caption className="carouselText">
                         <h3>Gwinnett Brawl Monthly Tournament</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                    <img id="carouselPic" alt="The Weekly Dojo" src={gbAd} />
-                    <Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className="carouselPic" alt="The Weekly Dojo" src={gbAd} />
+                    <Carousel.Caption className="carouselText">
                         <h3>The Weekly Dojo</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                    <img id="carouselPic" alt="The Road to FR" src={gbAd} />
-                    <Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className="carouselPic" alt="The Road to FR" src={gbAd} />
+                    <Carousel.Caption className="carouselText">
                         <h3>"The Road to ..."" Series</h3>
                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                     </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+                </Carousel.Item>
+            </Carousel>
+        </CarouselStyle>
     );
 };
 
