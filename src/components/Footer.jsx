bb44ae716c1cfd83b258427f70e22dd1f25ce0f5
sillah2010 <macktastic2014@gmail.com>
2018-03-14 17:53:01 -0400

    Added icons for social media

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
-//import Link from 'react-router-dom'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom';
+
+
+const IconStyle = styled.div`
+    ul {
+        list-style: none;
+        display: flex;
+        justify-content: center;
+        li {
+            padding: 0 20px;
+        }
+    };
+`
 
 const Footer = () => {
     return (
         <div>
-            <p>Follow us Social Media!</p>
-            <div className="fb-page" data-href="https://www.facebook.com/GBrawl/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="false" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/GBrawl/" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/GBrawl/">GB League</a></blockquote></div>
-            <p>Gwinnett Brawl, LLC</p>
+            <h3>Follow us on: </h3>
+            <IconStyle>
+                <ul>
+                    <li><Link target="_blank" to="https://www.facebook.com/gwinnettbrawl"><i className="fa fa-facebook"></i></Link></li>
+                    <li><Link target="_blank" to="https://twitter.com/gbdojo"><i className="fa fa-twitter"></i></Link></li>
+                    <li><Link target="_blank" to="https://twitch.tv/gbdojo"><i className="fa fa-twitch"></i></Link></li>
+                </ul>
+            </IconStyle>
+            <h3>Gwinnett Brawl, LLC</h3>
         </div>
     );
 };
